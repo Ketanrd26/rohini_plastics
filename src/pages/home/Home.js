1aa1eec9b82bab6d274afbc82c3bdc6ac65591d1
Ketan Dudka <ketandudka566123@gmail.com>
2024-12-21 13:23:31 +0530

    push

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { SwiperSlide } from "swiper/react";
 import Herosection from "../../comps/herosection/Herosection";
 import "./Home.scss";
+
 const Home = () => {
   return (
     <>
@@ -15,8 +16,8 @@ const Home = () => {
         <div class="mini-about-cont cont">
           <div class="left">
             <div class="div"></div>
-            <div class="div"></div>
-            <div class="div"></div>
+            <div class="div2 bg-img-cover"></div>
+            <div class="div3 bg-img-cover"></div>
           </div>
 
           <div class="right">
@@ -33,13 +34,49 @@ const Home = () => {
             </p>
 
             <div class="btn">
-              <div class="inner_text">
-                Know More
+              <div class="inner_text">Know More</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* activities */}
+
+      <div class="activities_parent parent">
+        <div class="activities_cont cont">
+          <h1>Activities</h1>
+
+          <div class="card_list">
+            <div class="card">
+              <div class="left">
+                <div class="icon"></div>
+              </div>
+              <div class="content">
+                <h3>ENGINEERING PLASTICS</h3>
+                <p>
+                  We are engaged in development & supply of various types of
+                  plastic engineering parts.
+                </p>
+              </div>
+            </div>
+            <div class="card">
+              <div class="left">
+                <div class="icon"></div>
+              </div>
+              <div class="content">
+                <h3>ENGINEERING PLASTICS</h3>
+                <p>
+                  We are engaged in development & supply of various types of
+                  plastic engineering parts.
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
+
+
+      {/* our values */}
     </>
   );
 };
